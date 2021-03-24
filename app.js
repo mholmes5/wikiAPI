da5d97beec4117227530287b1842999c31739d50
Michael Holmes <mholmes5@asu.edu>
2021-03-23 20:08:21 -0500

    Add articles routes and get single article route

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles",function(req,res){
+/////// Requests targeting all articles ///////
+
+app.route("/articles")
+
+  .get(function(req,res){
 
     Article.find({},function(err, articles){
       if(!err){
@@ -28,7 +32,51 @@ app.get("/articles",function(req,res){
         res.send(err);
       }
     });
-});
+  })
+  .post(function(req,res){
+
+    const article = new Article ({
+      title: req.body.title,
+      content: req.body.content
+    });
+
+    article.save(function(err){
+      if(!err){
+        res.send("Article successfully added");
+      }else{
+        res.send(err);
+      }
+    });
+  })
+  .delete((req, res)=>{
+
+    Article.deleteMany({},(err)=>{
+      if(!err){
+        res.send("All Articles successfully deleted");
+      }else{
+        res.send(err);
+      }
+    });
+  });
+
+
+/////// Requests targeting single article ///////
+
+app.route("/articles/:articleTitle")
+
+  .get((req,res)=>{
+    Article.findOne({title:req.params.articleTitle},(err, foundArticle)=>{
+      if(!err){
+        if(foundArticle){
+          res.send(foundArticle);
+        }else{
+          res.send("No article with title " +req.params.articleTitle +" was found.");  
+        }
+      }else{
+        res.send(err);
+      }
+    })
+  });
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
