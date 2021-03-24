7359efa6100abd93a1c3457ac9b64bc6a599f364
Michael Holmes <mholmes5@asu.edu>
2021-03-23 22:06:33 -0500

    Add single article routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const app = express();
 
@@ -42,6 +43,7 @@ app.route("/articles")
 
     article.save(function(err){
       if(!err){
+        res.status(201);
         res.send("Article successfully added");
       }else{
         res.send(err);
@@ -52,6 +54,7 @@ app.route("/articles")
 
     Article.deleteMany({},(err)=>{
       if(!err){
+        res.status(200);
         res.send("All Articles successfully deleted");
       }else{
         res.send(err);
@@ -65,14 +68,76 @@ app.route("/articles")
 app.route("/articles/:articleTitle")
 
   .get((req,res)=>{
+
     Article.findOne({title:req.params.articleTitle},(err, foundArticle)=>{
       if(!err){
         if(foundArticle){
+          res.status(200);
           res.send(foundArticle);
         }else{
-          res.send("No article with title " +req.params.articleTitle +" was found.");  
+          res.status(404);
+          res.send("No article with title " +req.params.articleTitle +" was found.");
         }
       }else{
+        res.send(err);
+      }
+    })
+  })
+  .put((req,res)=>{
+    Article.updateOne(
+      {title:req.params.articleTitle},
+      {title:req.body.title, content:req.body.content},
+
+      function(err, updatedArticle){
+        if(!err){
+          if(updatedArticle.n === updatedArticle.nModified && updatedArticle.n === 1){
+            res.status(200);
+            res.send("Article Updated");
+          }else{
+            res.status(404);
+            res.send("Article not updated");
+          }
+        }else{
+          res.status(500);
+          res.send(err);
+        }
+      }
+    )
+  })
+  .patch((req,res)=>{
+    Article.updateOne(
+      {title:req.params.articleTitle},
+      req.body,
+
+      function(err, updatedArticle){
+        if(!err){
+          if(updatedArticle.n === updatedArticle.nModified && updatedArticle.n === 1){
+            res.status(200);
+            res.send("Article Updated");
+          }else{
+            res.status(404);
+            res.send("Article not updated");
+          }
+        }else{
+          res.status(500);
+          res.send(err);
+        }
+      }
+    )
+  })
+  .delete((req,res)=>{
+    Article.deleteOne({title:req.params.articleTitle},(err, deletion)=>{
+      if(!err){
+        if(deletion.n === deletion.deletedCount && deletion.n ===1){
+          res.status(200);
+          res.send(req.params.articleTitle +" Article successfully deleted");
+        }else{
+          res.status(404);
+          res.send(req.params.articleTitle +" Article does not exist");
+        }
+
+      }else{
+
         res.send(err);
       }
     })
